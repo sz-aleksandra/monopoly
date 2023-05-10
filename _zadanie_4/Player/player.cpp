8efe24f931bbfdadad2ff2bfbf623bcb0587d788
Jakub Bąba <bombix.git@gmail.com>
2023-05-11 00:58:37 +0200

    Implementation of basic methods

---
+++ _zadanie_4/Player/player.cpp
@@ -0,0 +1,69 @@
+// Project: Monopoly
+// Author: Jakub Bąba
+
+#include "player.h"
+
+Player::Player(std::string name_input) {
+    name = name_input;
+    money = 1500;
+    location = 0;
+    alive = True;
+}
+
+void Player::change_name(std::string name_input) {
+    name = name_input
+}
+
+void Player::add_money(int amount) {
+    money += amount;
+}
+
+void Player::take_money(int amount) {
+    money -= amount;
+}
+
+void Player::move_player(int amount) {
+    // assuming that size is 40
+    location = (location + amount) % 40;
+}
+
+void Player::set_location(int new_location) {
+    location = new_location;
+}
+
+void Player::add_property(int index) {
+    properties.insert(index);
+}
+
+void Player::remove_property(int index) {
+    properties.erase(index);
+}
+
+
+
+std::string Player::get_name() {
+    return name;
+}
+
+int Player::get_money() {
+    return money;
+}
+
+int Player::get_location() {
+    return location;
+}
+
+bool Player::has_property(int index) {
+    return properties.find(index) != properties.end();
+}
+
+bool Player::is_alive() {
+    return alive;
+}
+
+
+
+void Player::pay_to_other(Player receiver, int amount) {
+    take_money(amount);
+    receiver.add_money(amount);
+}
--- _zadanie_4/Player/player.h
+++ _zadanie_4/Player/player.h
@@ -2,32 +2,34 @@
 // Author: Jakub Bąba
 
 #pragma once
+#include <set>
 
 class Player {
 private:
     std::string name;
     int money;
     int position;   // on board
-    vector<int> properties;
+    std::set<int> properties;
     bool alive;
 
 public:
     Player(std::string name_input);
 
-    void change_name(string inputName);
-    void change_money(int amount);
+    void change_name(std::string name_input);
+    void add_money(int amount);
+    void take_money(int amount);
     void move_player(int amount);
     void set_location(int new_location);
     void add_property(int index);
     void remove_property(int index);
 
-    string get_name();
+    std::string get_name();
     int get_money();
     int get_location();
     bool has_property(int index);
     bool is_alive();
 
     // interactions
-    void pay_to_other(Player receiver);
+    void pay_to_other(Player receiver, int amount);
 
 };
