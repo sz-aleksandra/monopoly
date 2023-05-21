7af32ac7b6176d678674a100f38516e78569f49a
Jakub Bąba <bombix.git@gmail.com>
2023-05-21 14:06:39 +0200

    Copied Player classes from task 4

---
+++ Player/player.cpp
@@ -0,0 +1,110 @@
+// Project: Monopoly
+// Author: Jakub Bąba
+
+#include "player.h"
+
+Player::Player(std::string name_input) {
+    name = std::move(name_input);
+    money = 1500;
+    position = 0;
+    in_jail = false;
+    alive = true;
+}
+
+/* ---------- CHANGERS ---------- */
+void Player::change_name(std::string name_input) {
+    if (!name_input.empty())
+        name = std::move(name_input);
+    else if (DEBUG)
+        std::cout << "Name cannot be empty\n";
+}
+
+void Player::add_money(int amount) {
+    money += amount;
+}
+
+void Player::take_money(int amount) {
+    if (amount <= money)
+        money -= amount;
+    else {
+        if (DEBUG)
+            std::cout << "Tried to remove more money than player has\n";
+        // interaction to start selling Player stuff to be implemented
+    }
+}
+
+void Player::move_player(int amount) {
+    // assuming that size is 40
+    while (position + amount < 0)
+        position += 40;
+    position = (position + amount) % 40;
+}
+
+void Player::set_position(int new_location) {
+    if (new_location < 40 && new_location >= 0)
+        position = new_location;
+    else if (DEBUG)
+        std::cout << "Tried to move outside board\n";
+}
+
+void Player::add_property(int index) {
+    if (DEBUG && has_property(index))
+        std::cout << "Player already has this property\n";
+    properties.insert(index);
+}
+
+void Player::remove_property(int index) {
+    if (DEBUG && !has_property(index))
+        std::cout << "Player didn't have this property\n";
+    properties.erase(index);
+}
+
+
+/* ---------- GETTERS ---------- */
+std::string Player::get_name() {
+    return name;
+}
+
+int Player::get_money() {
+    return money;
+}
+
+int Player::get_position() {
+    return position;
+}
+
+bool Player::has_property(int index) {
+    return properties.find(index) != properties.end();
+}
+
+bool Player::is_in_jail() {
+    return in_jail;
+}
+
+bool Player::is_alive() {
+    return alive;
+}
+
+
+/* ---------- INTERACTIONS ---------- */
+void Player::pay_to_other(Player receiver, int amount) {
+    take_money(amount);
+    receiver.add_money(amount);
+}
+
+void Player::buy_property(int index, int price) {
+    take_money(price);
+    add_property(index);
+}
+
+void Player::player_description() {
+    std::cout << "Player name: " << name;
+    std::cout << "\nMoney amount: " << money;
+    std::cout << "\nPosition on board (index): " << position;
+    std::cout << "\nIs alive: " << ( (alive) ? "Yes" : "No" );
+    std::cout << "\nIs in jail: " << ( (in_jail) ? "Yes" : "No" );
+    std::cout << "\nOwned properties (indexes): ";
+    for (auto i : properties)
+        std::cout << i << " ";
+    std::cout << "\n\n";
+}
---
+++ Player/player.h
@@ -0,0 +1,65 @@
+// Project: Monopoly
+// Author: Jakub Bąba
+
+#pragma once
+#include <iostream>
+#include <set>
+
+class Player {
+protected:
+    std::string name;
+    int money;
+    int position;   // on board
+    std::set<int> properties;
+    bool in_jail;
+    bool alive;
+
+public:
+    Player(std::string name_input);
+
+    // setters and typical changing properties
+    virtual void change_name(std::string name_input);
+    void add_money(int amount);
+    void take_money(int amount);
+    void move_player(int amount);
+    void set_position(int new_location);
+    void add_property(int index);
+    void remove_property(int index);
+
+    // getters
+    std::string get_name();
+    int get_money();
+    int get_position();
+    bool has_property(int index);
+    bool is_in_jail();
+    bool is_alive();
+
+    // interactions
+    void pay_to_other(Player receiver, int amount);
+    void buy_property(int index, int price);
+    // interacting with other classes, not implementing for now
+    void take_mortgage(int index, int price);
+    void pay_off_mortgage(int index, int price);
+    void buy_house(int index, int price);
+    void buy_hotel(int index, int price);
+
+    // miscellaneous
+    void player_description();
+};
+
+class Human : public Player {
+private:
+    bool pass_login;
+    std::string password;
+public:
+    Human(std::string name_input, std::string set_pass="");
+    void set_password(std::string new_p);
+    void change_password(std::string old_p, std::string new_p);
+    void remove_password(std::string pass);
+};
+
+class Bot : public Player {
+public:
+    Bot(std::string name_input);
+    void change_name(std::string name_input);
+};
