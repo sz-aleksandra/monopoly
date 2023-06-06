a564d2577925ebcf0adcc6cab76f5625f87fae2b
Jakub Bąba <bombix.git@gmail.com>
2023-06-07 01:02:41 +0200

    deleted some trash comments

--- Player/player_driver.cpp
+++ Player/player_driver.cpp
@@ -6,11 +6,9 @@
 #endif
 #include "player_driver.h"
 
-// @TODO check if unit is player type
 PlayerDriver::PlayerDriver(Player &unit, Hand &roller, Board &board, Deck<Card> &chances, Deck<Card> &chests, vector<Player> &players) : player(unit), hand(roller), board(board), deck_chance(chances), deck_chest(chests), all_players(players), go_out_chances(0) {}
 
 void PlayerDriver::make_turn() {
-    // @TODO think about better way to do that, maybe with try
     clear_screen();
     board.printBoard();
     std::cout << "\n------------------------------\n";
